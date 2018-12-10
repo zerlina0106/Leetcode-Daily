accf76d02008143c60836536e56fd4a56336ee26
zerlina0106 <1050670814@qq.com>
2018-12-10 20:53:44 +0800

    solved 687 QAQ

--- LC_easy/src/LongestUnivaluePath_687/Solution.java
+++ LC_easy/src/LongestUnivaluePath_687/Solution.java
@@ -13,35 +13,42 @@ public class Solution {
        TreeNode(int x) { val = x; }
    }
     int maxPath = 0;
+	//返回值就是代表当前节点开始往下延伸,最长路径的值,注意不包括回来的值,就是只遍历一边的节点
 	public int nodeTraverse(TreeNode node){
-		int leftPath = 0;
-		int rightPath = 0;
-		if(node.left == null && node.right == null){
-			return leftPath + rightPath;
+		//如果这个节点是叶子节点,就返回0
+		if(node == null || node.left == null && node.right == null){
+			return 0;
 		}
 
-		if(node.left != null){
-			if(node.val == node.left.val){
-				leftPath = nodeTraverse(node.left) + 1;
-			}else {
-				nodeTraverse(node.left);
-			}
+		int leftPath;
+		int rightPath;
+		// 用递归的时候,就把这个函数当成已经完成的并且可以返回正确的值
+		// 这里面就是,把nodeTraverse看做是一个可以正确返回这个节点最长路径的函数
+		// leftValue 就是,左子节点往下延伸时候的最长路径,right同理
+		int leftValue = nodeTraverse(node.left);
+		int rightValue = nodeTraverse(node.right);
+		// leftPath代表是从当前节点开始,从左子节点开始递归的时候的最大长度
+		// 如果当前左子节点的值和自己的值是相等的,那么leftPath就是左子节点的值+1
+		// 如果左子节点的值和自己当前的值不相等的话,那么leftPath的值就应该是0
+		if(node.left != null && node.val == node.left.val){
+			leftPath = leftValue + 1;
+		} else {
+			leftPath = 0;
 		}
-
-		if(node.right != null){
-			if(node.val == node.right.val){
-				rightPath = nodeTraverse(node.right) + 1;
-			}else {
-				nodeTraverse(node.right);
-			}
+		// 右子节点同样
+		if(node.right != null && node.val == node.right.val){
+			rightPath = rightValue + 1;
+		} else {
+			rightPath = 0;
 		}
-
-
+		// 如果左右都不是0,就代表左右子节点的值和自己的值都是相等的,那么相加就是代表连通起来的值
 		if((leftPath+rightPath) >= maxPath){
 			maxPath = leftPath+rightPath;
 		}
+		// 返回左右path的最大值,就代表当前节点延下遍历最长的路径的值
 		return Math.max(leftPath,rightPath);
 	}
+
 	public int longestUnivaluePath(TreeNode root) {
 		if(root != null){
 			nodeTraverse(root);
