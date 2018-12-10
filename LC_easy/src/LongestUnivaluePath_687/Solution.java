12dcb111827979b18ee59b076ff7d6fa8e950247
zerlina0106 <1050670814@qq.com>
2018-12-10 11:16:08 +0800

    confused by left and right path

--- LC_easy/src/LongestUnivaluePath_687/Solution.java
+++ LC_easy/src/LongestUnivaluePath_687/Solution.java
@@ -3,6 +3,8 @@ package LongestUnivaluePath_687;
 
 import org.junit.Test;
 
+
+
 public class Solution {
 	public class TreeNode {
        int val;
@@ -10,53 +12,55 @@ public class Solution {
        TreeNode right;
        TreeNode(int x) { val = x; }
    }
-	static  int maxPath;
-	public int longestUnivaluePath(TreeNode root, int curPath){
-		if(root==null || (root.left==null && root.right==null)){
-			return maxPath;
+    int maxPath = 0;
+	public int nodeTraverse(TreeNode node){
+		int leftPath = 0;
+		int rightPath = 0;
+		if(node.left == null && node.right == null){
+			return leftPath + rightPath;
 		}
-		if(root.left!= null && root.left.val == root.val ){
-			curPath++;
-			maxPath = Math.max(maxPath,curPath);
-			longestUnivaluePath(root.left, curPath);
 
-		}else if(root.right!=null && root.right.val == root.val) {
-
-				curPath++;
-				maxPath = Math.max(maxPath, curPath);
-				longestUnivaluePath(root.right, curPath);
-			} else {
-				curPath = 0;
-				longestUnivaluePath(root.left, curPath);
-				longestUnivaluePath(root.right, curPath);
+		if(node.left != null){
+			if(node.val == node.left.val){
+				leftPath = nodeTraverse(node.left) + 1;
+			}else {
+				nodeTraverse(node.left);
 			}
+		}
 
-		return maxPath;
+		if(node.right != null){
+			if(node.val == node.right.val){
+				rightPath = nodeTraverse(node.right) + 1;
+			}else {
+				nodeTraverse(node.right);
+			}
+		}
+
+
+		if((leftPath+rightPath) >= maxPath){
+			maxPath = leftPath+rightPath;
+		}
+		return Math.max(leftPath,rightPath);
 	}
 	public int longestUnivaluePath(TreeNode root) {
-		int  curPath = 0;
-		if(root==null){
-			return 0;
+		if(root != null){
+			nodeTraverse(root);
 		}
-		if(root.right==null && root.left==null){
-			return 0;
-		}
-		return  longestUnivaluePath(root, curPath);
-
+		return maxPath;
 	}
 	@Test
 	public void main(){
-		TreeNode root = new TreeNode(5);
-		TreeNode l1 = new TreeNode(5);
-//		TreeNode r1 = new TreeNode(5);
-//		TreeNode l2 = new TreeNode(1);
-//		TreeNode r20 = new TreeNode(1);
-//		TreeNode r21 = new TreeNode(5);
-//		root.right = r1;
-		root.right = l1;
-//		l1.left = l2;
-//		l1.right = r20;
-//		r1.right = r21;
+		TreeNode root = new TreeNode(1);
+		TreeNode l1 = new TreeNode(1);
+		TreeNode r1 = new TreeNode(1);
+		TreeNode l2 = new TreeNode(3);
+		TreeNode r20 = new TreeNode(1);
+		TreeNode r21 = new TreeNode(5);
+		root.right = r1;
+		root.left = l1;
+		l1.left = l2;
+		l1.right = r20;
+		r1.left = r21;
 		System.out.println(longestUnivaluePath(root));
 
 	}
