ad0e8baccb92d529d2748bbce763ed985e1e8a2a
zerlina0106 <1050670814@qq.com>
2018-12-11 16:06:18 +0800

    update 70 and 83 ^v^

---
+++ LC_easy/src/ClimbingStairs_70/BruteForce.java
@@ -0,0 +1,19 @@
+package ClimbingStairs_70;
+
+public class BruteForce {
+	public class Solution {
+		public int climbStairs(int n) {
+			return climb_Stairs(0, n);
+		}
+		public int climb_Stairs(int i, int n) {
+			if (i > n) {
+				return 0;
+			}
+			if (i == n) {
+				return 1;
+			}
+			return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+		}
+	}
+
+}
