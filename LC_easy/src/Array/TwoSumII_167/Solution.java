f2b6e64ecc65d4fd7c50911d0ef62f85ef622e0b
zerlina0106 <1050670814@qq.com>
2018-12-19 21:28:37 +0800

    167_the solution of discussion...awesome

---
+++ LC_easy/src/Array/TwoSumII_167/Solution.java
@@ -0,0 +1,22 @@
+package Array.TwoSumII_167;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public int[] twoSum(int[] numbers, int target) {
+		int l=0,r=numbers.length-1;
+		while(numbers[l] + numbers[r] != target){
+			if(numbers[l]+numbers[r] > target) r--;
+			else l++;
+		}
+		return new int[]{l+1,r+1};
+	}
+	@Test
+	public void main(){
+		int[] a = {2,3,4};
+		twoSum(a,6);
+	}
+}
