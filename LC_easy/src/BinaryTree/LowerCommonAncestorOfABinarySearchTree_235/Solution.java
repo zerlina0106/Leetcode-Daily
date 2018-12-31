05f2304adf0b45290ce396bde9f2d156c7513c24
zerlina0106 <1050670814@qq.com>
2018-12-31 14:39:14 +0800

    ... ...

---
+++ LC_easy/src/BinaryTree/LowerCommonAncestorOfABinarySearchTree_235/Solution.java
@@ -0,0 +1,41 @@
+package BinaryTree.LowerCommonAncestorOfABinarySearchTree_235;
+
+import javax.swing.tree.TreeCellRenderer;
+
+public class Solution {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+
+	}
+
+
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+			// Value of current node or parent node.
+			int parentVal = root.val;
+
+			// Value of p
+			int pVal = p.val;
+
+			// Value of q;
+			int qVal = q.val;
+
+			if (pVal > parentVal && qVal > parentVal) {
+				// If both p and q are greater than parent
+				return lowestCommonAncestor(root.right, p, q);
+			} else if (pVal < parentVal && qVal < parentVal) {
+				// If both p and q are lesser than parent
+				return lowestCommonAncestor(root.left, p, q);
+			} else {
+				// We have found the split point, i.e. the LCA node.
+				return root;
+			}
+		}
+
+}
