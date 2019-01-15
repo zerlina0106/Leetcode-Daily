01b3ca4edd4a09d1223a9f44bbe7beb4276d021e
zerlina0106 <1050670814@qq.com>
2019-01-15 11:21:02 +0800

    ....

---
+++ LC_easy/src/BinaryTree/BinaryTreePath_257/Solution.java
@@ -0,0 +1,15 @@
+package BinaryTree.BinaryTreePath_257;
+
+import java.util.List;
+
+public class Solution {
+	public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+   }
+//	public List<String> binaryTreePaths(TreeNode root) {
+//
+//	}
+}
