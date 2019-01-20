a34c9d6b65454b4bb0465fb68141cfa471fad0c6
zerlina0106 <1050670814@qq.com>
2019-01-20 23:43:18 +0800

    ...

--- LC/src/BinaryTree/BinaryTreePath_257/Solution.java
+++ LC/src/BinaryTree/BinaryTreePath_257/Solution.java
@@ -1,5 +1,6 @@
 package BinaryTree.BinaryTreePath_257;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
@@ -9,7 +10,25 @@ public class Solution {
        TreeNode right;
        TreeNode(int x) { val = x; }
    }
-//	public List<String> binaryTreePaths(TreeNode root) {
-//
-//	}
+    List<String> ans = new LinkedList<>();
+    public List<String> recur(TreeNode root, String path){
+        if(root.left==null && root.right==null){
+            ans.add(path);
+            return ans;
+        }
+        if(root.left != null){
+            recur(root.left,path + "->" + root.left.val);
+
+        }
+        if(root.right != null){
+            recur(root.right,path+ "->" + root.right.val);
+
+        }
+        return ans;
+    }
+	public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null) return ans;
+        else
+        return recur(root,root.val+"");
+	}
 }
